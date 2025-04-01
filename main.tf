e6299e9de1aa6788d31b92ceb33be1128a03703f
raghavgupta0 <102575786+raghavgupta0@users.noreply.github.com>
2025-04-01 11:00:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,5 +11,6 @@ provider "grafana" {
    alias = "cloud"
 
    url   = "https://raghavgupta418.grafana.net/"
-   auth  = "glc_eyJvIjoiMTM4OTk4MiIsIm4iOiJncmFmYW5hLWdyYWZhbmEiLCJrIjoiVU43Szg4NktIVzUwOUQ4SElaaDlJdjhUIiwibSI6eyJyIjoicHJvZC1hcC1zb3V0aC0xIn19"
+   #auth  = "glc_eyJvIjoiMTM4OTk4MiIsIm4iOiJncmFmYW5hLWdyYWZhbmEiLCJrIjoiVU43Szg4NktIVzUwOUQ4SElaaDlJdjhUIiwibSI6eyJyIjoicHJvZC1hcC1zb3V0aC0xIn19"
+   auth = "glc_eyJvIjoiMTM4OTk4MiIsIm4iOiJ0ZXN0LXRlc3QiLCJrIjoiM2kxQjdzODByMTZaWEE1V2lOdTAyeHpRIiwibSI6eyJyIjoidXMifX0="
 }
